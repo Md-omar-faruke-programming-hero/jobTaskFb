4e25779de4f550ddbd1ad8548386d4d99f66380b
plcpl-faruke <md.faruke@profecialinks.com>
2022-09-01 16:59:49 +0600

    share function implementation done

--- src/Component/HomePage/Homepage.js
+++ src/Component/HomePage/Homepage.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import LeftSideContent from '../LeftSideContent/LeftSideContent';
-import NavBar from '../NavBar/NavBar';
+import React from "react";
+import LeftSideContent from "../LeftSideContent/LeftSideContent";
+import MiddleContent from "../MiddleContent/MiddleContent";
+import NavBar from "../NavBar/NavBar";
 
 const Homepage = () => {
-    return (
-        <div className='bg-[#f0f2f5]'>
-            <NavBar></NavBar>
-            <LeftSideContent></LeftSideContent>
+  return (
+    <div className="bg-[#f0f2f5]">
+      <NavBar></NavBar>
+      <div className="flex  flex-row">
+        <div className="basis-1/4 md:block hidden">
+          <LeftSideContent></LeftSideContent>
         </div>
-    );
+        <div className="md:basis-1/2 basis-auto overflow-y-scroll h-screen">
+          <MiddleContent></MiddleContent>
+        </div>
+        <div className="basis-1/4 md:block hidden">
+          <LeftSideContent></LeftSideContent>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
--- src/Component/MiddleContent/MiddleContent.js
+++ src/Component/MiddleContent/MiddleContent.js
@@ -11,14 +11,30 @@ import { BsHandThumbsUp } from "react-icons/bs";
 import { BiComment } from "react-icons/bi";
 import { RiShareForwardLine } from "react-icons/ri";
 import { BsHandThumbsUpFill } from "react-icons/bs";
+import { MdOutlineNoteAlt } from "react-icons/md";
+import swal from "sweetalert";
 
 const MiddleContent = () => {
   // like button function
   const [like, setLike] = useState(false);
+  // comment button function
+  const [comentSectionShow, setComentSectionShow] = useState(false);
+  // share button function
+  const [shareSectionshowHide, setShareSectionshowHide] = useState(false);
+  const [shareCount, setShareCount] = useState(1);
+  const share = () => {
+    swal({
+      title: "Shared to your profile!",
+      icon: "success",
+      button: "Aww yiss!",
+    });
+    setShareSectionshowHide(!shareSectionshowHide);
+    setShareCount(shareCount + 1);
+  };
   return (
-    <div className="flex justify-center bg-[#f0f2f5] py-[50px]">
+    <div className="flex justify-center  pt-[50px] pb-[120px]">
       <div>
-        <div className="w-[500px] h-[auto] min-w-[auto] max-w-[100%] bg-white px-[15px] py-[10px] rounded-[10px] shadow-sm mb-[10px]">
+        <div className="md:w-[500px]  h-[auto] min-w-[auto] max-w-[100%] bg-white px-[15px] py-[10px] rounded-[10px] shadow-sm mb-[10px]">
           <div className="flex items-center">
             <img
               src={spiderMan}
@@ -26,7 +42,7 @@ const MiddleContent = () => {
               className="w-[40px] h-[40px] flex justify-center items-center rounded-[100px] mr-[10px]"
             />
             <input
-              className="bg-[#f0f2f5] w-full focus:outline-none rounded-[100px] px-[12px] py-[8px]"
+              className="bg-[#f0f2f5] w-full  focus:outline-none rounded-[100px] px-[12px] py-[8px]"
               type="text"
               name=""
               id=""
@@ -34,7 +50,7 @@ const MiddleContent = () => {
             />
           </div>
           <hr className="my-[14px]" />
-          <div className="grid md:grid-cols-3">
+          <div className="grid md:grid-cols-3 grid-cols-3 ">
             <div className="flex items-center px-[15px] py-[10px] hover:bg-[#f0f2f5] cursor-pointer rounded-[10px]">
               <AiOutlineVideoCamera className="w-[24px] h-[24px] mr-[5px] text-[#f3425f]"></AiOutlineVideoCamera>
               <p className="text-[15px] font-semibold text-[#65676b]">Live video</p>
@@ -49,7 +65,7 @@ const MiddleContent = () => {
             </div>
           </div>
         </div>
-        <div className="w-[500px] h-[auto] min-w-[auto] max-w-[100%] bg-white rounded-[10px] shadow-sm mb-[10px]">
+        <div className="md:w-[500px] h-[auto] min-w-[auto] max-w-[100%] bg-white rounded-[10px] shadow-sm mb-[10px]">
           <div className=" px-[15px] py-[10px] ">
             <div className="flex items-center justify-between">
               <div>
@@ -97,7 +113,7 @@ const MiddleContent = () => {
               <div>
                 <div className="flex items-center">
                   <p className="mr-[8px]">123 coments</p>
-                  <p>2 shares</p>
+                  <p>{shareCount} shares</p>
                 </div>
               </div>
             </div>
@@ -105,7 +121,7 @@ const MiddleContent = () => {
 
             <hr className="" />
             {/* like comment share buttons start*/}
-            <div className="grid md:grid-cols-3 gap-0 py-[3px]">
+            <div className="grid md:grid-cols-3 grid-cols-3 gap-0 py-[3px] relative">
               <button
                 onClick={() => setLike(!like)}
                 className="flex items-center px-[40px] py-[10px] hover:bg-[#f0f2f5] cursor-pointer rounded-[10px] mx-[auto]"
@@ -122,18 +138,44 @@ const MiddleContent = () => {
                   </>
                 )}
               </button>
-              <button className="flex items-center px-[30px] py-[10px] hover:bg-[#f0f2f5] cursor-pointer rounded-[10px] mx-[auto]">
+              <button
+                onClick={() => setComentSectionShow(true)}
+                className="flex items-center px-[30px] py-[10px] hover:bg-[#f0f2f5] cursor-pointer rounded-[10px] mx-[auto]"
+              >
                 <BiComment className="w-[24px] h-[24px] mr-[5px] text-[#606770]"></BiComment>
                 <p className="text-[15px] font-semibold text-[#606770]">Comment</p>
               </button>
-              <button className="flex items-center px-[20px] py-[10px] hover:bg-[#f0f2f5] cursor-pointer rounded-[10px] mx-[auto]">
+              <button
+                onClick={() => setShareSectionshowHide(!shareSectionshowHide)}
+                className="flex items-center px-[20px] py-[10px] hover:bg-[#f0f2f5] cursor-pointer rounded-[10px] mx-[auto] "
+              >
                 <RiShareForwardLine className="w-[40px] h-[24px] mr-[5px] text-[#606770]"></RiShareForwardLine>
                 <p className="text-[15px]   font-semibold text-[#606770]">Share</p>
               </button>
+              <div
+                className={`bg-white md:w-[360px] h-[120px] rounded-[12px] px-[16px] py-[18px] absolute md:top-[60px] md:right-[-60px] right-[0px] bottom-[-390px]  ${
+                  shareSectionshowHide ? "block" : "hidden"
+                } `}
+              >
+                <button
+                  onClick={share}
+                  className="flex items-center w-full py-[10px] hover:bg-[#f0f2f5] cursor-pointer rounded-[10px]"
+                >
+                  <RiShareForwardLine className="w-[40px] h-[24px]  text-[#606770]"></RiShareForwardLine>
+                  <p className="text-[15px]   font-semibold text-[#606770]">Share now (Public)</p>
+                </button>
+                <button
+                  onClick={share}
+                  className="flex items-center w-full py-[10px] hover:bg-[#f0f2f5] cursor-pointer rounded-[10px]"
+                >
+                  <MdOutlineNoteAlt className="w-[40px] h-[24px]  text-[#606770]"></MdOutlineNoteAlt>
+                  <p className="text-[15px]   font-semibold text-[#606770]">Share to Feed </p>
+                </button>
+              </div>
             </div>
             {/* like comment share buttons end */}
             {/* comment section start */}
-            {false ? (
+            {comentSectionShow ? (
               <div>
                 <hr className="pb-[5px]" />
                 <div className="flex items-center mt-[5px]">
@@ -150,7 +192,7 @@ const MiddleContent = () => {
                     placeholder="Write a comment..."
                   />
                 </div>
-                <div className="pb-[10px]">
+                <div className="pb-[10px] flex items-center justify-between">
                   <div className="flex items-center mt-[5px]">
                     <img
                       src={spiderMan}
@@ -161,6 +203,9 @@ const MiddleContent = () => {
                       <p className="font-semibold text-[14px]">Md.Omar Faruke</p>
                       <p className="font-normal text-[16px]">wish you good luck</p>
                     </div>
+                  </div>
+                  <div className="cursor-pointer">
+                    <BsThreeDots></BsThreeDots>
                   </div>
                 </div>
               </div>
