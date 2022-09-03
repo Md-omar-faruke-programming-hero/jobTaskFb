fe8ed74c0064b6047170752dda813c8e94fe2e99
plcpl-faruke <md.faruke@profecialinks.com>
2022-09-03 23:37:16 +0600

    build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Homepage from "./Component/HomePage/Homepage";
-import RightSideContent from "./Component/RightSideContent/RightSideContent";
 
 function App() {
   return (
@@ -9,10 +8,6 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Homepage></Homepage>
-          </Route>
-
-          <Route exact path="/right">
-            <RightSideContent></RightSideContent>
           </Route>
         </Switch>
       </BrowserRouter>
